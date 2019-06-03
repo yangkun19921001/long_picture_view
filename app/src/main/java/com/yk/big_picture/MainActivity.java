03d8def5c0dd63a957edbe11ae0b1e7c6ea47b25
阳坤 <yang1001yk@gmail.com>
2019-06-03 17:50:24 +0800

    上传捕获崩溃功能

--- app/src/main/java/com/yk/big_picture/MainActivity.java
+++ app/src/main/java/com/yk/big_picture/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.yk.big_picture_library.BigView;
 import com.yk.big_picture_library.LoadNetImageCallBack;
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
      * 网络图片地址
      */
     private String URL = "https://file.digitaling.com/eImg/uimages/20170104/1483513576654699.jpg";
+    private String URL_2 = "http://77fkxu.com1.z0.glb.clouddn.com/20180131/1517367951_59939.png";
 
 
     private String TAG = this.getClass().getSimpleName();
@@ -26,11 +28,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //如果测试过程中发生崩溃，请把日志告诉我，谢谢！
+        CrashHandler.getInstance().init(getApplicationContext());
         setContentView(R.layout.activity_main);
-
         BigView bigView=findViewById(R.id.bv_img);
-
-
         //本地文件
 /*        try {
             bigView.setImage(BIG_IMAGE_PAHT);
@@ -40,9 +41,8 @@ public class MainActivity extends AppCompatActivity {
         final ProgressDialog progressDialog = new ProgressDialog(this);
         progressDialog.setMessage("加载中....");
         progressDialog.setMax(100);
-
         //网络地址
-        bigView.setNetUrl(URL, new LoadNetImageCallBack() {
+        bigView.setNetUrl(URL_2, new LoadNetImageCallBack() {
             @Override
             public void onStart() {
                 progressDialog.show();
@@ -55,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onLoadFail(Exception e) {
-
+                Log.i(TAG,e.getMessage());
             }
 
             @Override
